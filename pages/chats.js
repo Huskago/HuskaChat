4ac4448bb62dcb33486f398a0c8d30d08c0756f3
Huskago <63999302+Huskago@users.noreply.github.com>
2022-03-20 22:51:07 +0100

    Update chats.js

--- pages/chats.js
+++ pages/chats.js
@@ -12,7 +12,6 @@ const MessageFormSocial = dynamic(() =>
   import("react-chat-engine").then((module) => module.MessageFormSocial)
 );
 
-
 export default function Chats() {
   const { username, secret } = useContext(Context)
   const [ showChat, setShowChat ] = useState(false)
